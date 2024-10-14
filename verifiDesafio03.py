c204ced5838ebf9e4fba7060daa8d52bc84ac680
Lipe <84376560+LipeRllk@users.noreply.github.com>
2024-10-13 21:46:05 -0300

    add verificação do desafio 03

---
+++ verifiDesafio03.py
@@ -0,0 +1,15 @@
+#verificando resultado do desafio 03
+
+# Inicializando as variáveis
+INDICE = 12
+SOMA = 0
+K = 1
+
+# Loop enquanto K for menor que INDICE
+while K < INDICE:
+    K = K + 1  # Incrementa K
+    SOMA = SOMA + K  # Adiciona K a SOMA
+    print(SOMA)
+
+# Imprime o resultado final
+print("resultado final: ",SOMA)
